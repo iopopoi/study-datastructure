894263b2a2060a0272edfffb2f923827b45823c2
iopopoi <kimkin0668@gmail.com>
2022-04-28 18:29:12 +0900

    ERROR

--- MENTOR/SW/16235.cpp
+++ MENTOR/SW/16235.cpp
@@ -6,18 +6,33 @@
 */
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <queue>
+#include <string.h>
 
 using namespace std;
 
 int N, M, K;  
 int A[11][11]; // 겨울에 받는 영양분
 int Map[11][11]; // map의 각지역의 영양분을 말한다.
+int dir[8][2] = {{1,0},{0,1},{-1,0},{0,-1},{-1,-1},{-1,1},{1,-1},{1,1}};
+int yy, xx;
 
-struct TREE{
-    int y, x, age;
-};
+void Spring(){
+    
+}
 
-vector<TREE> forest;
+void Summer(){
+
+}
+
+void Fall(){
+
+}
+
+void Winter(){
+
+}
 
 int main(){
     cin >> N >> M >> K;
@@ -31,10 +46,20 @@ int main(){
     int x, y, z;
     for(int i=0;i<M;i++){
         cin >> y >> x >> z;
-        forest.push_back({y,x,z});
-        cout << forest[i].y << forest[i].x << forest[i].age << "\n";
     }
-
     // 가장 처음에 양분은 모든 칸에 5만큼 들어있다.
     memset(Map, 5,sizeof(Map));
+
+    for(int i=0;i<K;i++){
+        Spring();
+        Summer();
+        Fall();
+        Winter();
+    }
+    int cnt = 0;
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
+        }
+    }
+    cout << cnt;
 }
